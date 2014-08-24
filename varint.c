dbe0b981d9ad116b71b0b1fc3e0ce212ae428e04
Eduardo Sorribas <eduardo@sorribas.org>
2014-08-24 14:57:54 +0200

    Buffer checks.

--- varint.c
+++ varint.c
@@ -1,15 +1,17 @@
+#include <assert.h>
 #include "varint.h"
 #define NULL 0
 
 static char MSB = 0x80;
 static char MSBALL = ~0x7F;
 
-char* varint_encode(long long n, char* buf, unsigned char* bytes) {
+char* varint_encode(long long n, char* buf, int len, unsigned char* bytes) {
   char* ptr = buf;
 
   while (n & MSBALL) {
     *(ptr++) = (n & 0xFF) | MSB;
     n = n >> 7;
+    assert((ptr - buf) < len);
   }
   *ptr = n;
   if (bytes != NULL) *bytes = ptr - buf + 1;
@@ -17,15 +19,17 @@ char* varint_encode(long long n, char* buf, unsigned char* bytes) {
   return buf;
 }
 
-long long varint_decode(char* buf) {
+long long varint_decode(char* buf, int len) {
   long long result = 0;
   int bits = 0;
   long long ll;
+  int i = 0;
   while (*buf & MSB) {
     ll = *buf;
     result += ((ll & 0x7F) << bits);
     buf++;
     bits += 7;
+    assert(++i < len);
   }
   ll = *buf;
   result += ((ll & 0x7F) << bits);
--- varint.h
+++ varint.h
@@ -1,7 +1,7 @@
 #ifndef _VARINT_H_
 #define _VARINT_H_
 
-char* varint_encode(long long, char*, unsigned char*);
-long long varint_decode(char*);
+char* varint_encode(long long, char*, int, unsigned char*);
+long long varint_decode(char*, int);
 
 #endif
